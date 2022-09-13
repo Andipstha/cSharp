5ce9190d8e551e6381505b78752cb86943e0e1d7
andipstha <sandip.stha120@gmail.com>
2022-09-14 00:48:30 +0545

    added hello dir

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
