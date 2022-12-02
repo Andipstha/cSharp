cceae9f03922d2b4732c30b7dca16f01410c3173
andipstha <sandip.stha120@gmail.com>
2022-12-02 08:40:09 +0545

    update

--- employee/employee/Form1.cs
+++ employee/employee/Form1.cs
@@ -50,7 +50,7 @@ namespace employee
         }
 
         private void displayData_Click(object sender, EventArgs e)
-        {
+        {   /*
             SqlConnection con = new SqlConnection(@"Data Source = DESKTOP-TSU4EHG\SQLEXPRESS;
                                                 Initial Catalog=empl_details;
                                                 user id=sa;password=kist@123;");
@@ -68,7 +68,30 @@ namespace employee
             dataGridView1.DataSource = table;
 
             con.Close();
+            */
+            try
+            {
+                string query = "Select * from employee";
+                SqlCommand sqlCommand = new SqlCommand(query, con);
+                SqlDataAdapter sda = new SqlDataAdapter(sqlCommand);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                // dataGridView1.DataSource = dt;
+                int sn = 1;
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    string id = dt.Rows[i]["column1"].ToString();
+                    string name = dt.Rows[i]["column2"].ToString();
+                    string address = dt.Rows[i]["column3"].ToString();
+                    string salary = dt.Rows[i]["column4"].ToString();
+                    dataGridView1.Rows.Add(sn++, id, name, address, salary);
 
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error : " + ex.InnerException);
+            }
         }
 
         private void btnUpdate_Click(object sender, EventArgs e)
@@ -95,7 +118,7 @@ namespace employee
         private void btnInsert_Click(object sender, EventArgs e)
         {
             con.Open();
-            string query = "Insert into student " + "(name,address,contact,college)" + "values(@student_name,@address,@salary)";
+            string query = "Insert into student " + "(name,address,salary)" + "values(@name,@address,@salary)";
 
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandText = query;
